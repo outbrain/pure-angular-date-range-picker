d91b8d0614bae2f4585c90ebe76e839e942547b3
Vlad Ioffe <vlio20@gmail.com>
2015-11-01 08:33:53 +0200

    added back button logic to calender dir

--- src/app/directives/calendar/calendar.directive.js
+++ src/app/directives/calendar/calendar.directive.js
@@ -4,12 +4,15 @@ export function Calendar() {
   let directive = {
     restrict: 'E',
     scope: {
-      'minMonth': '@',
-      'maxMonth': '@'
+      weekStart: '@',
+      minMonth: '&',
+      maxMonth: '&',
+      month: '='
+
     },
     templateUrl: 'app/directives/calendar/calendar.html',
     controller: CalendarController,
-    controllerAs: 'calendar',
+    controllerAs: 'month',
     bindToController: true
   };
 
@@ -21,6 +24,66 @@ class CalendarController {
     'ngInject';
 
     this.Moment = moment;
+
+    this.firstDayOfWeek = this.weekStart ? this.weekStart : 'su';
+    this.daysOfWeek = this.buildWeek(this.firstDayOfWeek);
+    this.calendar = this.buildCalendar(this.month);
   }
 
+  buildWeek(firstDay) {
+    let daysOfWeek = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
+    let pivot = daysOfWeek.indexOf(firstDay.toLowerCase());
+    let firstHalf = daysOfWeek.slice(0, pivot);
+    let secondHalf = daysOfWeek.slice(pivot, daysOfWeek.length);
+    let week = secondHalf.concat(firstHalf);
+
+    return week;
+  }
+
+  buildCalendar(date = this.Moment()) {
+    let monthWeeks = [[], [], [], [], [], []];
+    let monthRange = this.getMonthDateRange(date.year(), date.month() + 1);
+    let firstDayOfMonth = monthRange.start;
+
+    let pivot = this.daysOfWeek.indexOf(firstDayOfMonth.format('dd').toLowerCase());
+    let tmpDate = firstDayOfMonth.clone().subtract(pivot, 'd');
+
+    for (let i = 0; i < 6; i++) {
+      for(let j = 0; j < 7; j++) {
+        monthWeeks[i][j] = tmpDate;
+        tmpDate = tmpDate.clone().add(1, 'd');
+      }
+    }
+
+    return {
+      currentCalendar: date,
+      selectedDate: date,
+      year: date.year(),
+      month: date.format('MMMM'),
+      firstDayOfMonth: monthRange.start.format('D'),
+      lastDayOfMonth: monthRange.end.format('D'),
+      monthWeeks: monthWeeks
+    }
+  }
+
+  moveCalenderByMonth(months) {
+    let mo = this.calendar.currentCalendar;
+    this.calendar = this.buildCalendar(mo.clone().add(months, 'M'));
+  }
+
+  getMonthDateRange(year, month) {
+    let startDate = this.Moment([year, month - 1]);
+    let endDate = this.Moment(startDate).endOf('month');
+    return {start: startDate, end: endDate};
+  }
+
+  showBackButton() {
+    let toShow = true;
+    if(this.minMonth && this.minMonth()) {
+      let minMonth = this.minMonth();
+      toShow = this.calendar.currentCalendar.diff(minMonth, 'month') > 1;
+    }
+
+    return toShow;
+  }
 }
