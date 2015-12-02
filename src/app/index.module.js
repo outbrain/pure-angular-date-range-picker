e03d8ecf82b402b5e8f51737f3727f5c2c21aa92
Vlad Ioffe <vlio20@gmail.com>
2015-12-02 22:51:51 +0200

    removed demo code from build

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,7 +1,6 @@
 /* global moment:false */
 
 import { config } from './index.config';
-import { MainController } from './main/main.controller';
 import { DateRangePicker } from './directives/date-range-picker/date-range-picker.directive';
 import { Calendar } from './directives/calendar/calendar.directive';
 import { ObDateRangePicker } from './directives/ob-date-range-picker/ob-date-range-picker.directive.js';
@@ -9,9 +8,6 @@ import { ObDateRangePicker } from './directives/ob-date-range-picker/ob-date-ran
 angular.module('obDateRangePicker', [])
   .constant('moment', moment)
   .config(config)
-
-  .controller('MainController', MainController)
-
   .directive('dateRangePicker', DateRangePicker)
   .directive('obDaterangepicker', ObDateRangePicker)
   .directive('calendar', Calendar);
